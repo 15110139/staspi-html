2cd0d7c6d4ebe2fa7bc119a0660baa314ea19df7
DESKTOP-7CL0DD2\95vin <95vinhndu1415@gmail.com>
2022-10-25 14:45:19 +0700

    push with err of pdf cant scale follow popup

--- JavaScript/show_pdf.js
+++ JavaScript/show_pdf.js
@@ -29,17 +29,74 @@ $(function () {
             },
             
             open: function () {
-              var windowWidth = window.innerWidth *0.79;
-              var windowHeight = window.innerHeight * 0.5;
-              var embed = '<iframe ';
-                  embed += 'src="';
-                  embed += '../pdf/' + fileName + '#toolbar=0' + '" ';
-                  // embed += 'type = "' + 'application/pdf" ';
-                  embed += 'height ="'+ windowHeight + '"' + ' width ="' + windowWidth + '"' + '>'
-                  embed += '</iframe>'
-                  console.log(embed);
+              // var windowWidth = window.innerWidth *0.79;
+              // var windowHeight = window.innerHeight * 0.5;
+              // var embed = '<iframe ';
+              //     embed += 'src="';
+              //     embed += '../pdf/' + fileName + '#toolbar=0' + '" ';
+              //     // embed += 'type = "' + 'application/pdf" ';
+              //     embed += 'height ="'+ windowHeight + '"' + ' width ="' + windowWidth + '"' + '>'
+              //     embed += '</iframe>'
+              //     console.log(embed);
 
-              $("#dialog").html(embed);
+              // $("#dialog").html(embed);
+
+                var url = '../pdf/' +fileName;
+                console.log(url);
+                var pdfjsLib = window['pdfjs-dist/build/pdf'];
+                pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.js';
+
+                var pdfDoc = null,
+                    pageNum = 1,
+                    pageRendering = false,
+                    pageNumPending = null,
+                    scale = 1.17;
+                function renderPage(num, canvas) {
+                  var ctx = canvas.getContext('2d');
+                  pageRendering = true;
+                  // Using promise to fetch the page
+                  pdfDoc.getPage(num).then(function(page) {
+                    var viewport = page.getViewport({scale: scale});
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+
+                    // Render PDF page into canvas context
+                    var renderContext = {
+                      canvasContext: ctx,
+                      viewport: viewport
+                    };
+                    var renderTask = page.render(renderContext);
+
+                    // Wait for rendering to finish
+                    renderTask.promise.then(function() {
+                      pageRendering = false;
+                      if (pageNumPending !== null) {
+                        // New page rendering is pending
+                        renderPage(pageNumPending);
+                        pageNumPending = null;
+                      }
+                    });
+                  });
+                }
+
+                pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+                  pdfDoc = pdfDoc_;
+
+                  const pages = parseInt(pdfDoc.numPages);
+
+                  var canvasHtml = '';
+                  for (var i = 0; i < pages; i++) {
+                    canvasHtml += '<canvas id="canvas_' + i + '"></canvas>';
+                  }
+                  console.log("check",canvasHtml);
+                  document.getElementById('dialog').innerHTML = canvasHtml;
+
+                  for (var i = 0; i < pages; i++) {
+                    var canvas = document.getElementById('canvas_' + i);
+                    renderPage(i+1, canvas);
+                  }
+                });
+
             },
           });
         });
