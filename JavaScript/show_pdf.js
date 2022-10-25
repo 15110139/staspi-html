f331a8d8a3772bdc075ce82dca690508260817f1
DESKTOP-7CL0DD2\95vin <95vinhndu1415@gmail.com>
2022-10-25 10:59:50 +0700

    push for fix responsive pdf

--- JavaScript/show_pdf.js
+++ JavaScript/show_pdf.js
@@ -5,11 +5,11 @@ $(function () {
         var fileName = val+ ".pdf";
           $("#dialog").dialog({
             modal: true,
-            width: "100%",
+            width: "80%",
             height: 500,
             // top:100,
             buttons: {
-              "tải về": function () {
+              "Tải về": function () {
                 var url = "../pdf/" + fileName;
                 var req = new XMLHttpRequest();
                 req.open("GET", url , true);
@@ -29,26 +29,17 @@ $(function () {
             },
             
             open: function () {
-              // var object =
-              //   '<object data="{FileName}#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf"  style = " "position:absolute; left: 0; top: 0;" width="100%" height="100%"">';
-              // object +=
-              //   'If you are unable to view file, you can download from <a href="{FileName}">here</a>';
-              // object +=
-              //   ' or download <a target = "_blank" href = "http://get.adobe.com/reader/">Adobe PDF Reader</a> to view the file.';
-              // object += "</object>";
-              // object = object.replace(/{FileName}/g, "../pdf/" + fileName);
-              // console.log(object);
-              // var embed = '<embed ';
-              //     embed += 'src="';
-              //     embed += '../pdf/' + fileName + '#toolbar=0' + '" ';
-              //     embed += 'type = "' + 'application/pdf" ';
-              //     embed += 'height ="100%" ' + 'width ="100%"' + '>'
-              //     embed += '</embed>'
-              //     console.log(embed);
+              var windowWidth = window.innerWidth *0.79;
+              var windowHeight = window.innerHeight * 0.5;
+              var embed = '<iframe ';
+                  embed += 'src="';
+                  embed += '../pdf/' + fileName + '#toolbar=0' + '" ';
+                  // embed += 'type = "' + 'application/pdf" ';
+                  embed += 'height ="'+ windowHeight + '"' + ' width ="' + windowWidth + '"' + '>'
+                  embed += '</iframe>'
+                  console.log(embed);
 
-              // $("#dialog").html(embed);
-              jQuery('embed#pdf_file').attr('src', '../pdf/' + fileName + "#toolbar=0");
-              // document.title("staspi");
+              $("#dialog").html(embed);
             },
           });
         });
