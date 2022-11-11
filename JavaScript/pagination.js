203546283abf5f09331e5cc0e393bfae09832e72
DESKTOP-7CL0DD2\95vin <95vinhndu1415@gmail.com>
2022-11-11 20:48:02 +0700

    push

--- JavaScript/pagination.js
+++ JavaScript/pagination.js
@@ -2,64 +2,64 @@ let perPage = 6;
 let idPage = 1;
 let start = 0;
 let end = perPage;
+const host = "http://localhost:1337";
+var listItems = [];
+const getUser = async () => {
+    try{
+        await fetch(`${host}/api/Posts?populate=thumbImage`).then(
+            res => {
+                return res.json();
+            }
+        ).then(
+            data => {
+                var topic = document.getElementById("topic");
+                var items = data.data;
+                let totalPages = Math.ceil(items.length / perPage);
+                listItems.push(items);
+                var load = "<div class= 'load-wrap'>"
+                load += "<div class = 'load'>";
+                load += "<div class= 'line'></div>";
+                load += "<div class= 'line'></div>";
+                load += "<div class= 'line'></div>";
+                load += "</div>";
+                load += '</div>';
+                // console.log("check load item",load);
+                topic.innerHTML = load;
+                setTimeout(()=> {
+                    initRender(items, totalPages); 
+                },1500)
+                
+            }
+        )
+    }catch(e){
+        console.log(e);
+    }
+    
+}
+getUser();
 
-const items = [
-    {
-        id: 1,
-        image: ".././images/blog/cover-image/content-marketting-anhbia.png",
-        title: "CONTENT MARKETING LÀ GÌ? Ý TƯỞNG VIẾT CONTENT MARKETING THU HÚT KHÁCH HÀNG",
-        content:
-            "Content marketing là gì? Viết content marketing như thế nào để thú hút khách hàng? Cùng Staspi tìm hiểu những thông tin thú vị về content marketing trong bài viết dưới đây nhé!",
-        link: "'/page/blogs/content-marketing-la-gi-y-tuong-viet-content-marketing-thu-hut-khach-hang.html'",
-    },
-    {
-        id: 2,
-        image: ".././images/blog/cover-image/content-seo-anhbia.png",
-        title: "LÀM THẾ NÀO ĐỂ VIẾT CONTENT SEO: HƯỚNG DẪN VIẾT CONTENT CHUẨN SEO VỚI 5 BƯỚC ĐƠN GIẢN",
-        content: "Để một bài đăng được lên top tìm kiếm quả thật rất khó, bởi bài viết của bạn phải cạnh tranh với nhiều đối thủ khác trên thị trường. Bên cạnh những thủ thuật thì bài viết chuẩn SEO cũng là một trong những yêu cầu tiên quyết để leo rank trong cuộc đua xếp hạng tìm kiếm. Vậy làm thế nào để viết Content SEO? Trong bài viết này, Staspi sẽ hướng dẫn bạn cách viết content chuẩn SEO chỉ với 5 bước đơn giản có thể áp dụng được ngay.",
-        link: "'/page/blogs/lam-the-nao-de-viet-content-seo-huong-dan-viet-content-chuan-seo-voi-5-buoc-don-gian.html'",
-    },
-    {
-        id: 3,
-        image: ".././images/blog/cover-image/social-media-anh-bia.jpg",
-        title: "TỔNG HỢP KÍCH THƯỚC CHUẨN HÌNH ẢNH TRÊN CÁC KÊNH SOCIAL MEDIA",
-        content: "Social Media là nền tảng mang đến nhiều tiềm năng phát triển cho doanh nghiệp. Khi biết cách tối ưu hình ảnh chuẩn trên Social Media sẽ mang đến ấn tượng tốt và sự chuyên nghiệp đến với khách hàng. Và dưới đây là tổng hợp kích thước chuẩn hình ảnh trên các kênh Social Media, bạn nhớ lưu lại và áp dụng nhé!",
-        link: "'/page/blogs/tong-hop-kich-thuoc-chuan-hinh-anh-tren-cac-kenh-social-media.html'",
-    },
-    {
-        id: 4,
-        image: ".././images/blog/cover-image/meo-stnd-tiktok-anhbia.png",
-        title: "MẸO SÁNG TẠO NỘI DUNG TRÊN TIKTOK DỄ LÊN XU HƯỚNG",
-        content: "Tiktok là nền tảng mạng xã hội chia sẻ video ngắn phổ biến hiện nay, đây là sân chơi giải trí hấp dẫn đối với thế hệ Millennial và Gen Z. Nền tảng Tiktok cho thấy sự phát triển mạnh mẽ về độ phổ biến và số lượng người dùng gia tăng nhanh chóng vượt qua cả Facebook, Youtube, Instagram,.... Việc cho phép người dùng dễ dàng sáng tạo nội dung trên Tiktok đã góp phần lớn tạo nên sự thành công của nền tảng này. Vậy, giữa hàng ngàn video được đăng tải mỗi ngày thì làm sao để video của bạn nhận được nhiều sự quan tâm? Trong bài viết dưới đây, Staspi sẽ chia sẻ cho bạn mẹo sáng tạo nội dung trên Tiktok dễ lên xu hướng hiện nay nhé!",
-        link: "'/page/blogs/meo-sang-tao-noi-dung-tren-tik-tok-de-len-xu-huong.html'",
-    },
-    {
-        id: 5,
-        image: ".././images/blog/cover-image/tang-follow-tiktok-anhbia.png",
-        title: "10+ CÁCH TĂNG FOLLOW TIKTOK MIỄN PHÍ",
-        content: "Tiktok là một trong những ứng dụng phổ biến nhất hiện nay, mọi người từ bé đến lớn đều thích sử dụng nền tảng này. Với hơn 3 tỷ người sử dụng Tiktok, việc có nhiều người theo dõi có thể mở ra nhiều cơ hội cho nhà sáng tạo nội dung cũng như doanh nghiệp. Vậy làm thế nào để tăng follow Tiktok? Hãy cùng Staspi tham khảo ngay 10+ cách tăng follow Tiktok đơn giản, miễn phí sau đây nhé!",
-        link: "'/page/blogs/10-cach-tang-follow-tik-tok-mien-phi.html'",
-    },
-    {
-        id: 6,
-        image: ".././images/blog/cover-image/tao-video-tiktok-anhbia.png",
-        title: "HƯỚNG DẪN LÀM VIDEO TIKTOK BẰNG ĐIỆN THOẠI",
-        content: "Tiktok là sân chơi mới có số lượng người truy cập nhiều nhất hiện nay, người dùng có thể dễ dàng tham gia chia sẻ đa dạng nội dung thông qua dạng video ngắn. Nếu bạn chưa biết cách làm video tiktok, hãy tham khảo ngay bài chia sẻ dưới đây của Staspi.",
-        link: "'/page/blogs/huong-dan-lam-tik-tok-don-gian-bang-dien-thoai.html'",
-    },
-];
 
-// let itemArr = [];
+var keyword = document.getElementById('keyword');
+var but = document.getElementById("sb");
 
-let totalPages = Math.ceil(items.length / perPage);
-// var clearPageItem = () => {
-//     const prePage = document.querySelector('.topic .pagination .pre-page');
-//     const nxtPage = document.querySelector('.topic .pagination .nxt-page');
-//     if(totalPages === 0 || totalPages === 1){
-//         prePage.style.display = 'none';
-//         nxtPage.style.display = 'none';
-//      }
-// }
+var searching = (e) => {
+    e.preventDefault();
+    keySearch = keyword.value.toLowerCase();
+    if(listItems.indexOf(keySearch) === -1){
+        console.log("no match");
+    }else{
+        console.log("oke");
+    }
+}
+but.addEventListener("click",(e)=>searching(e));
+var clearPageItem = () => {
+    const prePage = document.querySelector('.topic .pagination .pre-page');
+    const nxtPage = document.querySelector('.topic .pagination .nxt-page');
+    if(totalPages === 0 || totalPages === 1){
+        prePage.style.display = 'none';
+        nxtPage.style.display = 'none';
+     }
+}
 
 function initRender(items, totalPage) {
     // clearPageItem();
@@ -67,7 +67,6 @@ function initRender(items, totalPage) {
     renderListPage(totalPage);
 }
 
-initRender(items, totalPages);
 
 function getCurrentPage(indexPage) {
     start = (indexPage - 1) * perPage;
@@ -80,12 +79,13 @@ getCurrentPage(1);
 function renderTopic(topic) {
     html = "";
     const content = topic.map((item, index) => {
+        var image = item.attributes.thumbImage.data.attributes.formats;
         if (index >= start && index < end) {
             html += '<div class="item-topic">';
-            html += '<a class = onclick-image href = ' + item.link + '>';
+            html += '<a class = onclick-image href = ' + "/page/blogs/" + item.attributes.slug + '>';
             html +=
-                "<img  src=" +
-                item.image +
+                "<img  src=" + host +
+                image.medium.url +
                 " class = " +
                 "'" +
                 "image-item" +
@@ -95,7 +95,7 @@ function renderTopic(topic) {
             // html += "<div class = 'show-more'>";
             // html += "<h5>Xem thêm</h5>";
             // html += "</div>";
-            html += "<a href =" + item.link + ">"
+            html += "<a href =" +"/page/blogs/" + item.attributes.slug + ">"
             html +=
                 "<div " +
                 // "onclick =" +
@@ -106,18 +106,18 @@ function renderTopic(topic) {
                 "overlay-item" +
                 "'" +
                 ">";
-            html += '<div class = "title-item">'+'<h4>' + item.title + '</h4>'+ "</div>";
-            html += '<p class = "content-item">' + item.content + "</p>";
-            html += '<a class = ' + "'navigation'" + 'href =' + item.link;
+            html += '<div class = "title-item">'+'<h4>' + item.attributes.title + '</h4>'+ "</div>";
+            html += '<p class = "content-item">' + item.attributes.subTitle + "</p>";
+            html += '<a class = ' + "'navigation'" + 'href ='+ "/page/blogs/" + item.attributes.slug;
             html += '>'
             html += 'Xem chi tiết'
             html += '</a>'
             html += "</div>";
             html += "</a>"
-            html += "<a href =" + item.link  + ">";
-            html += '<div class = "title-mobile" >' + item.title + "</div>";
+            html += "<a href =" + item.attributes.slug  + ">";
+            html += '<div class = "title-mobile" >' + item.attributes.title + "</div>";
             html += "</a>"
-            html += '<div class = "content-mobile">' + item.content + "</div>";
+            html += '<div class = "content-mobile">' + item.attributes.subTitle + "</div>";
             html += "</div>";
             return html;
         }
@@ -177,7 +177,7 @@ $(".nxt-page").on("click", () => {
     } else {
         $(".nxt-page").removeClass("disable");
     }
-    console.log(idPage);
+    // console.log(idPage);
     const btnPre = document.querySelector(".pre-page");
     btnPre.classList.remove("disable");
     $(".page-number li").removeClass("active");
