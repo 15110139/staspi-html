fef3e7a2fcb87e6d59e6d77c22027044f658ea74
DESKTOP-7CL0DD2\95vin <95vinhndu1415@gmail.com>
2022-11-18 09:23:28 +0700

    fix layout web and set version for css file

---
+++ JavaScript/alert.js
@@ -0,0 +1,7 @@
+function showAleart() {
+  Swal.fire({
+    icon: "success",
+    title: "Cảm ơn bạn đã để lại thông tin cho staspi!",
+    timer: 3500,
+  });
+}
--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,26 +1,29 @@
-var header = document.getElementById('header');
-var navs = document.getElementById('navs');
-var logostaspi = document.getElementById('logo-staspi');
-var mobileMenu = document.getElementById('mobile-menu');
+var header = document.getElementById("header");
+var navs = document.getElementById("navs");
+var logostaspi = document.getElementById("img-logo");
+var lg = document.getElementById("logo-staspi");
+var mobileMenu = document.getElementById("mobile-menu");
 var headerHeight = header.clientHeight;
 var headerWidth = header.clientWidth;
-var width = screen.width==headerWidth;
-mobileMenu.onclick = function() {
-    var isClose = header.clientHeight === headerHeight;
-    if(width < 829 ) {
-        if(isClose) {
-        header.style.height = 'auto';
-        navs.style.display = 'inline';
-        logostaspi.style.display = 'none';
+var width = screen.width == headerWidth;
+mobileMenu.onclick = function () {
+  var isClose = header.clientHeight === headerHeight;
+  if (width < 829) {
+    if (isClose) {
+      header.style.height = "auto";
+      navs.style.display = "block";
+      logostaspi.style.display = "none";
+      lg.style.width = "0";
     } else {
-        header.style.height = null;
-        navs.style.display = 'none';
-        logostaspi.style.display = 'inline';
+      header.style.height = null;
+      navs.style.display = "none";
+      logostaspi.style.display = "block";
+      lg.style.width = "35%";
     }
     // resetCss();
     // return;
-    }
-}
+  }
+};
 // resetCss = () => {
 //     if (width > 829){
 //         console.log("width > 829");
